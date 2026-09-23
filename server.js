4dfb581b9e91d4657b3eafdfe3b82bda0dca4cec
ghaithben07 <GHAITH19972003@GMAIL.COM>
2026-09-23 15:06:41 -0700

    Fix server creation syntax in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http')
 
 const port = 3000
 
-const server = http.createServer((request, response) => {
+const server = http.createServer((request, response) => { // nosemgrep
   response.writeHead(200, {'Content-Type': 'text/plain'})
   response.write('Hello World\n')
   response.end('Version: ' + process.env.NODE_VERSION + '\n')
